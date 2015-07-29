bf821b22ab19a71e5373bd168ef3954910cc90ea
jieunK <ji2un0317@gmailcom>
2015-07-29 21:03:11 +0900

    Create WordCount.rb

---
+++ WordCount.rb
@@ -0,0 +1,11 @@
+def WordCount(str)#코드가 짧아서 기분이 좋아졌다.헤헤
+
+  # code goes here
+  str = str.split
+  return str.length
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)           
