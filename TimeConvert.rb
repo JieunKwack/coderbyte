fabfad0ec9c53b242fb33f030e1c3c671bcc2457
jieunK <ji2un0317@gmailcom>
2015-07-30 13:12:41 +0900

    Update TimeConvert.rb

--- TimeConvert.rb
+++ TimeConvert.rb
@@ -14,3 +14,11 @@ end
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
 TimeConvert(STDIN.gets)           
+
+  #better way
+  #mins = num % 60
+  #hours = (num / 60)
+
+  
+  #num = hours.to_s + ":" + mins.to_s
+  
