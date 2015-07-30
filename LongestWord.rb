ae07599c27f077ca03ae537944de9a6dd84093bc
jieunK <ji2un0317@gmailcom>
2015-07-30 09:30:07 +0900

    Update LongestWord.rb

--- LongestWord.rb
+++ LongestWord.rb
@@ -1,13 +1,15 @@
- def LongestWord(sen)#site 안들어가짐/submit못함
-
+def LongestWord(sen)
   # code goes here
   arr = []
+  len_arr = []
   sen = sen.split
   sen.each {|x| arr = arr + x.gsub(/\W+/,'').to_a}
-  return arr.max
+  arr.each{|x| len_arr = len_arr + x.length.to_a}
+  len_arr = len_arr.index(len_arr.max)
+  return arr[len_arr]
          
 end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-LongestWord(STDIN.gets) 
+LongestWord(STDIN.gets)           
