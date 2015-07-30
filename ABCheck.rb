f9e93d9006327bff1014b5aa3c6a6fce24d4db5f
jieunK <ji2un0317@gmailcom>
2015-07-30 16:39:15 +0900

    Update ABCheck.rb

--- ABCheck.rb
+++ ABCheck.rb
@@ -1,21 +1,35 @@
-def ABCheck(str)#not complete
+def ABCheck(str)
 
   # code goes here
   arr = str.split("")
-  position_a = []
   position_b = []
   position = 0
   arr.each do |x|
-    if x == "a"
-      position_a += position.to_a 
-    elsif x == "b"
+    if x == "b"
       position_b += position.to_a
     end
     position += 1
   end
-  return position_a     
+    if position_b.empty?
+    return "false"
+  end
+  len_arr = arr.length
+  position_b.each do |x|
+    if (x+4 <= len_arr)
+      if (arr[x+4] == "a")
+      	return "true"
+      end
+    end
+    if (0 <= x-4)
+      if (arr[x-4] == "a")
+        return "true"
+      end
+    end
+  end
+  return "false"
+
 end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-ABCheck(STDIN.gets) 
+ABCheck(STDIN.gets)           
